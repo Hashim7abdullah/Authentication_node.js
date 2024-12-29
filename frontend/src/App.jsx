bb5f9cae57a11159a15fa80970f23fee7e643fff
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-12-30 00:51:46 +0530

    created frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,6 @@ const CreateUser = lazy(() => import("./Pages/CreateUser"));
 const UpdateUser = lazy(() => import("./Pages/UpdateUser"));
 const DeleteUser = lazy(() => import("./Pages/DeleteUser"));
 
-// Protected Route Component (you'll need to implement this)
 
 const App = () => {
   return (
